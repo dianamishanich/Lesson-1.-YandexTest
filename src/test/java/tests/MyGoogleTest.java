6cbf3fa987e09ca4173be7502f2b46a68f9781c1
dianamishanich <diana-mishanich@ya.ru>
2020-10-07 01:46:53 +0300

    YandexTest

--- src/test/java/tests/MyGoogleTest.java
+++ src/test/java/tests/MyGoogleTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -9,17 +8,18 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class YandexTest {
+
     @Test
-void selenideSearchTest() {
+    void selenideSearchTest() {
         // Открыть Yandex
-         open("https://yandex.ru");
+        open("https://yandex.ru");
 
+        // Ввести Autumn в поиск
+        $(byName("text")).setValue("Autumn").pressEnter();
 
-// Ввести Autumn в поиск
-                $(byName("text")).setValue("Autumn").pressEnter();
-// Проверить, что TEST появился в результатах поиска
-       $( "html").shouldHave(text("context.reverso.net"));
-}
+        // Проверить, что TEST появился в результатах поиска
+        $( "html").shouldHave(text("context.reverso.net"));
+    }
 
 
 }
